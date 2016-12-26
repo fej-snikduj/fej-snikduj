47c6d2f63800c30705f293c036b1743dd830d590
Jeff Judkins <judkinsj@gmail.com>
2016-12-26 03:39:57 -0800

    added profile box with flexbox styling

--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
 var express = require('express');
 var app = express();
 app.use(express.static('./client'));
-var server = app.listen(process.env.PORT || 3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Server listening at http://%s:%s', host, port);
-});
+var server = app.listen(process.env.PORT || 3000);
